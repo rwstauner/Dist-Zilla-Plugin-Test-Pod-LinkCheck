ac6257dcc1ee6abf9779a1c8e8d02b45c775f4c2
Randy Stauner <randy@magnificent-tears.com>
2011-04-11 16:03:26 -0700

    Add code and DATA section so that the module works

--- lib/Dist/Zilla/Plugin/Test/Pod/LinkCheck.pm
+++ lib/Dist/Zilla/Plugin/Test/Pod/LinkCheck.pm
@@ -4,6 +4,11 @@ package Dist::Zilla::Plugin::Test::Pod::LinkCheck;
 
 use strict;
 use warnings;
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
 
@@ -12,3 +17,26 @@ use warnings;
 =head1 DESCRIPTION
 
 =cut
+
+__DATA__
+___[ xt/release/pod-linkcheck.t ]___
+#!perl
+
+use strict;
+use warnings;
+use Test::More;
+
+foreach my $env_skip ( qw(
+  SKIP_POD_LINKCHECK
+) ){
+  plan skip_all => "\$ENV{$env_skip} is set, skipping"
+    if $ENV{$env_skip};
+}
+
+eval "use Test::Pod::LinkCheck";
+if ( $@ ) {
+  plan skip_all => 'Test::Pod::LinkCheck required for testing POD';
+}
+else {
+  Test::Pod::LinkCheck->new->all_pod_ok;
+}
